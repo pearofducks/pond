d9c2a804045e6fb0ec32ec63f3aecb3e70878a6e
pearofducks <pearofducks@gmail.com>
2014-08-06 07:18:09 +0200

    sort

--- generator.rb
+++ generator.rb
@@ -2,6 +2,7 @@ require 'haml'
 
 class Generator
   def self.generate! images
+    images.sort_by { |i| i.captured_at }
     input = images.first.src
     output = images.first.dest
     html = Haml::Engine.new(File.read(input + "templates/index.haml")).render(Object.new, {:images => images})
