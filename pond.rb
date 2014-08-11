0d8604381cca724623a0ba879f5723080e6c22f5
pearofducks <pearofducks@gmail.com>
2014-08-11 08:47:11 +0000

    bind

--- pond.rb
+++ pond.rb
@@ -1,23 +1,22 @@
 require 'bundler/setup'
-require 'micro_magick'
 require 'rainbow'
-require 'pry'
 require_relative 'image'
 require_relative 'generator'
 
 class Pond
   def self.go input, output
-    @input = File.expand_path input
-    @output = File.expand_path(output || '.')
+    @input = File.expand_path input rescue abort("Usage: pond INTPUT OUTPUT")
+    @output = File.expand_path output rescue abort("Usage: pond INTPUT OUTPUT")
     @images = []
-    puts Rainbow("\nInput is:").yellow + ' ' + Rainbow(@input).underline
-    puts Rainbow("Output is:").yellow + ' ' + Rainbow(@output).underline + "\n\n"
+    puts "\n\n"
+    puts Rainbow("Input is:").yellow + ' ' + Rainbow(@input).underline
+    puts Rainbow("Output is:").yellow + ' ' + Rainbow(@output).underline
     inputs = Dir.glob(@input + '**/*.jpg', File::FNM_CASEFOLD) # case-insensitive glob
+    puts Rainbow("Found #{inputs.length} images...").yellow
+    puts "\n\n"
     inputs.each do |unprocessed_file|
       @images << Image.new(unprocessed_file, @output)
     end
     Generator.generate! @images
   end
 end
-
-Pond.go ARGV[0], ARGV[1]
